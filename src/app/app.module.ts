b14cbd71cf8a5be595de4f167c11b61052306507
cr9169 <bar.gabay.home@gmail.com>
2025-02-07 13:17:01 +0200

    add dashboard, project list, project detail, and project create components with corresponding templates and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
+import { ProjectCreateComponent } from './components/project-create/project-create.component';
+import { ManagementServiceService } from './services/management-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProjectListComponent,
+    ProjectDetailComponent,
+    ProjectCreateComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [ManagementServiceService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
