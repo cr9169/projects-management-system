3b604fc77dc51a1c9b5fbfa6a17752da3e15579c
cr9169 <bar.gabay.home@gmail.com>
2025-02-07 14:30:13 +0200

    add routing for dashboard and project components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { ProjectCreateComponent } from './components/project-create/project-create.component';
+import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectListComponent,
+  },
+  {
+    path: 'projects/create',
+    component: ProjectCreateComponent,
+  },
+  {
+    path: 'projects/:id',
+    component: ProjectDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
